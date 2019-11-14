088769eac982e21d7cff93c859d62c7f1b938d9d
baihe <baihe@guahao.com>
2019-11-14 15:47:32 +0800

    报表相关接口接入

--- src/SponsoredProducts/Report/ServiceProvider.php
+++ src/SponsoredProducts/Report/ServiceProvider.php
@@ -9,7 +9,7 @@ class ServiceProvider implements ServiceProviderInterface
 {
     public function register(Container $pimple)
     {
-        $pimple['productTargeting'] = function ($app) {
+        $pimple['report'] = function ($app) {
             return new Client($app);
         };
     }
